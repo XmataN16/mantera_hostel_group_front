693e0dd4a22e80a29107548962c74740e26a809a
XmataN16 <chuykov.nik@mail.ru>
2026-09-23 15:36:58 +0300

    111

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api' // В продакшене будет проксироваться
+  apiUrl: 'https://mantera-backend.onrender.com' // В продакшене будет проксироваться
 };
